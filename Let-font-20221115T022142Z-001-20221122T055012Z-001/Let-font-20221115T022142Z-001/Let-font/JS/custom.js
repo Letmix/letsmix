b20785a0498d4ad05b775cfbdc66b229397cbb8b
Letmix <eiranghi0209@gmail.com>
2022-11-22 20:16:07 +0700

    hihi

---
+++ Let-font-20221115T022142Z-001-20221122T055012Z-001/Let-font-20221115T022142Z-001/Let-font/JS/custom.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+$('#text-truot').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+	dots:false,
+	autoplay:true,
+	autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
